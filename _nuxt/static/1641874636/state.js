3f0ab095eb22a794ccb182bb00533691409ed497
Tverous <Tverous@users.noreply.github.com>
2022-01-11 04:17:16 +0000

    deploy: 5fce96a5fde48390b4f99c53fb76e91a3fe5512c

---
+++ _nuxt/static/1641874636/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641874636",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"270a62ec"}}}}(null,"\u002F"));
