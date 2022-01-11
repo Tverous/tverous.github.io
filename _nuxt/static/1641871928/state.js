f080a5d06581f075ed9b646b163020430da9373d
Tverous <Tverous@users.noreply.github.com>
2022-01-11 03:39:49 +0000

    deploy: e63250931122ba1f463f2f24f2e54558c550ebf5

--- _nuxt/static/1641871928/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"tverous.github.io\u002F_nuxt\u002Fstatic\u002F1641871928",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"tverous.github.io\u002F",assetsPath:"tverous.github.io\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"687f5ffe"}}}}(null));
